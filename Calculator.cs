e54b5631f69221ddfb26a132e12e3d7742dc7825
Saad-hassan78 <saadhassan9577@gmail.com>
2025-03-06 10:06:45 +0500

    Update Calculator.cs

--- Calculator.cs
+++ Calculator.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Name: Muhammad Saad Hassan, Roll No: 24-ST-058");
+        Conflictbranch("Name: Muhammad Saad Hassan, Roll No: 24-ST-058");
 Subtraction
         // Declare two numbers for the operation
         int a = 10, b = 5;
@@ -26,6 +26,20 @@ main
         int a = 5, b = 3;
         int sum = a + b;
         Console.WriteLine("Sum: " + sum);
+ Division
+    // Declare two numbers for the operation
+        int a = 10, b = 5;
+
+        // Division operation (step 6 - division branch)
+        if (b != 0)
+        {
+            float quotient = (float)a / b;
+            Console.WriteLine("Quotient: " + quotient);
+        }
+        else
+        {
+            Console.WriteLine("Cannot divide by zero");
+
 
  main
 }
