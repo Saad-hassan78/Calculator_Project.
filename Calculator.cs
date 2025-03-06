74eef217254d49a75477eb9241ca76ec464379e7
Saad-hassan78 <saadhassan9577@gmail.com>
2025-03-06 09:44:57 +0500

    Added Multiplication Operation

--- Calculator.cs
+++ Calculator.cs
@@ -5,5 +5,11 @@ class Program
     static void Main()
     {
         Console.WriteLine("Name: Muhammad Saad Hassan, Roll No: 24-ST-058");
+   // Declare two numbers for the operation
+        int a = 10, b = 5;
+
+        // Multiplication operation (step 6 - multiplication branch)
+        int product = a * b;
+        Console.WriteLine("Product: " + product);
     }
 }
